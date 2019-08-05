147f9fef466461428409d4072d69bbf396480f24
jwan <ciwan_ali@hotmail.com>
2019-08-04 22:05:14 -0500

    added ex 27,28,29,30,31,32 exercises

---
+++ js/index31.js
@@ -0,0 +1,5 @@
+const firsSide = 10;
+const SecondSide = 20;
+const ThirdSide = 5;
+const perimiterOfTriangle = firsSide+SecondSide+ThirdSide ;
+console.log('perimiter of a triangle has lengths (20,10,5) is',perimiterOfTriangle);
