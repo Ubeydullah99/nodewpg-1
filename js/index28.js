8307505b349495405b470a378673b75b94643591
jwan <ciwan_ali@hotmail.com>
2019-08-06 18:28:41 -0500

    added some updating

--- js/index28.js
+++ js/index28.js
@@ -9,3 +9,7 @@ console.log('9times 7 equals',number*7);
 console.log('9times 8 equals',number*8);
 console.log('9times 9 equals',number*9);
 console.log('9times 10 equals',number*10);
+var i;
+for (i = 0; i <=10; i++) { console.log(`9times ${i} equals`,number*i);
+ 
+}
--- js/index32.js
+++ js/index32.js
@@ -2,4 +2,6 @@ const height = 10;
 const base = 4;
 const perimiterOfRectangular = (height+base)*2;
 const areaOfRectangular = height*base;
-console.log(`the perimiter of rectangular has a height = ${height} and base=${base} is: ${perimiterOfRectangular} and the area is: ${areaOfRectangular}`)
+console.log(`the perimiter of rectangular has a height = ${height}
+ and base=${base} is: ${perimiterOfRectangular}
+ and the area is: ${areaOfRectangular}`)
