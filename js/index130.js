9c25aa13295c870ea6e8f9c84b1e3694544c1a17
jwan <ciwan_ali@hotmail.com>
2019-08-19 00:40:14 -0500

    added  exercisses from index128.js to index132.js

---
+++ js/index130.js
@@ -0,0 +1,8 @@
+let mutants = ['Professor X', 'Cyclops', 'Iceman', 'Angel', 'Beast', 'Phoenix'];
+const sortedMutants = mutants.sort();
+const reversedSortedMutants = sortedMutants.reverse();
+const joinedMutants = mutants.join(' * ');
+console.log(mutants);
+console.log(sortedMutants);
+console.log(reversedSortedMutants);
+console.log(joinedMutants);
