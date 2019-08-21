3b64d861004025de49144a03102195de76b4ae92
jwan <ciwan_ali@hotmail.com>
2019-08-21 01:15:45 -0500

    added  exercisses from index148.js to index151.js

--- js/index144.js
+++ js/index144.js
@@ -9,3 +9,11 @@ user.username = user.username.toUpperCase();
 user.password = user.password.slice(0,6).concat(` ${user.password.substr(10,17)}`);
 console.log(user.username);
 console.log(user.password);
+const person = {
+    name: 'Nico',
+    age: 38
+  };
+  
+  console.log(person.name);
+  console.log(person.age);
+  console.log(person);
